ab60bd767880ac99490ebfa5d34ec744d86cb493
KamillaGazso <kamillagazso04@gmail.com>
2023-10-24 12:10:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using osztaly;
+using System.Globalization;
+
+namespace solsys
+{
+    internal class Program
+    {
+    
+        static void Main(string[] args)
+        {
+            List<osztalyos> bolygo = new List<osztalyos>();
+
+            foreach (var i in File.ReadAllLines(@"../../../src/solsys.txt"))
+            {
+                bolygo.Add(new osztalyos(i));
+            }
+
+            //bolygok szama
+            int osszbolygo = 0;
+            for (int j = 0; j < bolygo.Count; j++)
+            {
+                osszbolygo++;
+            }
+            Console.WriteLine($"Ennyi bolygó adata van a listában:{osszbolygo}");
+
+            //bolygók átlagos holdja
+            double atlaghoz = 0;
+            int ossz = 0;
+            for (int j = 0; j < bolygo.Count; j++)
+            {
+                atlaghoz += bolygo[j].HoldakSzama;
+                ossz++;
+            }
+            double szam = atlaghoz / ossz;
+            Console.WriteLine($"Ennyi a bolygók átlagos holdjának száma:{szam}");
+
+            //legnagyobb terfogatu bolygo
+            double max = 0;
+            string nev = string.Empty;
+            for (int j = 0; j < bolygo.Count; j++)
+            {
+                if (bolygo[j].TerfogatAranya>max)
+                {
+                    max = bolygo[j].TerfogatAranya;
+                }
+                if (bolygo[j].TerfogatAranya==max)
+                {
+                    nev = bolygo[j].BolygoNeve;
+                }
+            }
+            Console.WriteLine($"A maximum térfogatú bolygó neve:{nev}");
+
+            //bolygo neve
+            Console.Write("Írd be annak a bolygónak a nevét, amit keresel:");
+            string adottnev = Console.ReadLine();
+            for (int i = 0; i < bolygo.Count; i++)
+            {
+                if (bolygo[i].BolygoNeve.ToLower() == adottnev.ToLower())
+                {
+                    Console.WriteLine($"Van ilyen nevű bolygó: {bolygo[i].BolygoNeve}");
+                }
+            }
+
+            //tobb holdja van
+            Console.Write("Adj meg egy számot:");
+            int adott= Convert.ToInt32( Console.ReadLine());
+            for (int i = 0; i < bolygo.Count; i++)
+            {
+                if (bolygo[i].HoldakSzama>adott)
+                {
+                    Console.WriteLine($"Ennél több holdja van: {bolygo[i].BolygoNeve}");
+                }
+            }
+        }
+    }
+}
---
+++ osztaly.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztaly
+{
+    internal class osztalyos
+    {
+        public string BolygoNeve { get; set; }
+        public int HoldakSzama { get; set; }
+        public double TerfogatAranya { get; set; }
+
+        public osztalyos(string sor)
+        {
+            var v = sor.Split(";");
+            BolygoNeve = v[0];
+            HoldakSzama = Convert.ToInt32(v[1]);
+            TerfogatAranya = double.Parse(v[2]);
+
+        }
+        
+    }
+   
+}
